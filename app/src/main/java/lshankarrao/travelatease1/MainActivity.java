2839662a68f10863e2b9fa0cd1ac08a2b31ce61a
Lakshmisrao <lakshmisrao89@gmail.com>
2016-05-24 18:03:23 -0700

    Alarm Receiver + Main Activity

--- app/src/main/java/lshankarrao/travelatease1/MainActivity.java
+++ app/src/main/java/lshankarrao/travelatease1/MainActivity.java
@@ -1,7 +1,9 @@
 package lshankarrao.travelatease1;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button upcomingTrips = (Button) findViewById(R.id.button_upcomingTrips);
+        upcomingTrips.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TripListActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
